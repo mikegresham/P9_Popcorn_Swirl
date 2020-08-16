4dc31b54bb29b170bcc9d42e3ebf1bd4cb26c1ac
mikegresham <bac-UxE5>
2020-08-16 06:46:52 +0100

    Refactor

--- PopcornSwirlTests/DataManagerTestCase.swift
+++ PopcornSwirlTests/DataManagerTestCase.swift
@@ -90,5 +90,94 @@ class DataManagerTestCase: XCTestCase {
         DataManager.shared.deleteFilm(for: id)
     }
     
+    func testLazyList() {
+        //function should return an array of 20 placeholder items
+        
+        let list = DataManager.shared.filmList
+        
+        
+        //Result
+        print(list.count)
+        XCTAssert(list.count == 20)
+    }
+    
+    func testLazyGenre() {
+        //function should return list with first item as "Latest" genre
+        
+        let list = DataManager.shared.genreList
+        
+        //Result
+        print(list.count)
+
+        XCTAssert(list.first?.name == "Latest")
+    }
+    
+    func testCreateFilm() {
+        //test film is created and saved correctly.
+        //Setup
+        let id = 550 //RandomFilm ID
+        DataManager.shared.deleteFilm(for: id) //Ensure Film is not already stored
+        let filmBrief = FilmBrief(id: 550, title: "Film", posterPath: "url", notes: nil, bookmark: true, viewed: true)
+        
+        
+        let filmsBeforeTest = DataManager.shared.fetchFilmHistory()!.count
+        
+        // Create Film
+        DataManager.shared.updateFilm(film: filmBrief)
+        
+        let filmsAfterTest = DataManager.shared.fetchFilmHistory()!.count
+        
+        //Result
+        XCTAssert(filmsBeforeTest == filmsAfterTest - 1)
+        
+        //Clean Up
+        DataManager.shared.deleteFilm(for: id)
+        
+    }
+    
+    func testDeleteFilm() {
+        //test film is deleted and saved correctly.
+        //Setup
+        let id = 550 //RandomFilm ID
+        DataManager.shared.deleteFilm(for: id) //Ensure Film is not already stored
+        let filmBrief = FilmBrief(id: 550, title: "Film", posterPath: "url", notes: nil, bookmark: true, viewed: true)
+        // Create Film
+            
+        DataManager.shared.updateFilm(film: filmBrief)
+             
+        let filmsBeforeTest = DataManager.shared.fetchFilmHistory()!.count
+             
+        //Test
+        DataManager.shared.deleteFilm(for: id)
+        let filmsAfterTest = DataManager.shared.fetchFilmHistory()!.count
+             
+        //Result
+        XCTAssert(filmsBeforeTest == filmsAfterTest + 1)
+             
+        //Clean Up
+        DataManager.shared.deleteFilm(for: id)
+    }
+    
+    func testFetchHistory() {
+        //test fetch result.
+        //Setup
+        let id = 550 //RandomFilm ID
+        DataManager.shared.deleteFilm(for: id) //Ensure Film is not already stored
+        let filmBrief = FilmBrief(id: 550, title: "Film", posterPath: "url", notes: nil, bookmark: true, viewed: true)
+        
+        //Test
+        // Create Film
+        DataManager.shared.updateFilm(film: filmBrief)
+                    
+                    
+        let numberOfFilms = DataManager.shared.fetchFilmHistory()!.count
+    
+        //Result
+                    
+        XCTAssert(numberOfFilms > 0)
+                    
+        //Clean Up
+        DataManager.shared.deleteFilm(for: id)
+    }
 
 }
